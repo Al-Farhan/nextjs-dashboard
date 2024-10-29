5504cb90cbf4afad1d5d98537bc659e22efc5fe7
Farhan Shaikh <programmingwithfarhan@gmail.com>
2024-10-29 12:56:36 +0530

    fix:auth

--- auth.config.ts
+++ auth.config.ts
@@ -9,11 +9,12 @@ export const authConfig = {
             const isLoggedIn = !!auth?.user;
             const isOnDashboard = nextUrl.pathname.startsWith('/dashboard');
             if (isOnDashboard) {
-                if (isLoggedIn) {
-                    return Response.redirect(new URL('/dashboard', nextUrl));
-                }
-                return true;
+                if (isLoggedIn) return true;
+                return false; // Redirect unauthenticated users to login page
+            } else if (isLoggedIn) {
+                return Response.redirect(new URL('/dashboard', nextUrl));
             }
+            return true;
         }
     },
     providers: [],
